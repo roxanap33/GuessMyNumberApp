1e7aefafda24bda4fd845a18a8d22ec6510698e5
roxanap33 <roxana.ciocian@principal33.com>
2024-03-28 15:06:09 +0200

    63: Create the game screen and a reusable title component

--- starting-project/screens/GameScreen.js
+++ starting-project/screens/GameScreen.js
@@ -1,5 +1,21 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
 export default function GameScreen() {
-  return <Text>Game screen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      <View></View>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
